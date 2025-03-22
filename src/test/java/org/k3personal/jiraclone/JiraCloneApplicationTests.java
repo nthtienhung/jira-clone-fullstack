36991755985f2c0c47305fb3c8831552e22450b7
k3 <nthtienhung123@gmail.com>
2025-03-22 23:27:36 +0700

    first push

---
+++ src/test/java/org/k3personal/jiraclone/JiraCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.k3personal.jiraclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
